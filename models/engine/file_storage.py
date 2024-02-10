85885481b706972a882826e908ecda0ea4deecaa
Falcon <soufian9mkns@gmail.com>
2024-02-10 09:23:39 -0800

    pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,25 +3,28 @@ import json
 import os
 from models.base_model import BaseModel
 
+
 class FileStorage():
-    """serializeing instaces to a JSON file and deserializing JSON file to instances"""
+    """serializeing instaces to a JSON file and deserialization"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """ returns the dictionary __objects"""
         return self.__objects
+
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file (path: __file_path)"""
+        """ serializes __objects to the JSON file"""
         falcon = self.__objects
-        falcon_dictionary = {obj : falcon[obj].to_dict() for obj in falcon.keys()}
+        falcon_d = {obj: falcon[obj].to_dict() for obj in falcon.keys()}
         with open(self.__file_path, 'w') as file:
-            json.dump(falcon_dictionary, file)
+            json.dump(falcon_d, file)
+
     def reload(self):
         """deserializes the JSON file to __objects"""
         if os.path.exists(self.__file_path):
