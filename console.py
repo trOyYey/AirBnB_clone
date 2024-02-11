ca84db8067ec688b1665a0c8f9a1b4052fa83732
Falcon <soufian9mkns@gmail.com>
2024-02-10 23:57:32 -0800

    pycodestyle

--- console.py
+++ console.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import cmd
+
+
 class HBNBCommand(cmd.Cmd):
     """the entry point of the command interpreter"""
     prompt = "(hbnb) "
 
-    def do_quit(self,line):
+    def do_quit(self, line):
         """Quit command to exit the program"""
         return True
 
@@ -15,6 +17,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """empty line"""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
