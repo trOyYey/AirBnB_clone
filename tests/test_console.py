9c8c888a2ab5bb040d4837f5fa95d3bae7c33a4d
Falcon <soufian9mkns@gmail.com>
2024-02-11 07:43:15 -0800

    test_console

---
+++ tests/test_console.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Unittest for console.py"""
+
+import console
+import unittest
+
+
+class TestConsole(unittestTestCase):
+    """testing the console"""
+
+    @classmethod
+    def setUpClass(instance):
+        """setting variables for console """
+        instance.console = HBNBCommand()
+
+    @classmethod
+    def teardown(instance):
+        """removing setup variables"""
+        def instance.console
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_doc_strings(self):
+        """ everything is docummented??"""
+
+        sef.assertIsNotNone(console.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+
+    def test_emptyline(self):
+        """testing emptyline command"""
+
+        with patch("sys.stdout", new = stringIO()) as f:
+            self.console.onecmd("\n")
+            self.assertEqual(f.getvalue(), "")
+
