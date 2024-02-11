c83fdd632db4b837372eaf1b2499f6da450a6326
Falcon <soufian9mkns@gmail.com>
2024-02-11 06:43:40 -0800

    console

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ class HBNBCommand(cmd.Cmd):
     """the entry point of the command interpreter"""
     prompt = "(hbnb) " if sys.__stdin__.isatty() else ""
 
+    def preloop(self):
+        """Prints when isatty is false"""
+        if not sys.__stdin__.isatty():
+            print('(hbnb)')
+
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
