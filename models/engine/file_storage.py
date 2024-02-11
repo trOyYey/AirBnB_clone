89865492c5727a277f43f761378096d5694eef7e
trOyYeyY <troyzuwara@gmail.com>
2024-02-11 22:18:02 +0000

    tweaking storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,28 +15,29 @@ class FileStorage():
 
     def all(self):
         """ returns the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """ serializes __objects to the JSON file"""
-        falcon = self.__objects
-        falcon_d = {obj: falcon[obj].to_dict() for obj in falcon.keys()}
-        with open(self.__file_path, 'w') as file:
-            json.dump(falcon_d, file)
+        falcon = {}
+        for key in FileStorage.__objects:
+            falcon[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(falcon, file)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
             if os.path.exists(self.__file_path):
-                with open(self.__file_path) as file:
+                with open(FileStorage.__file_path) as file:
                     file_dictionary = json.load(file)
                     for key, value in file_dictionary.items():
                         attribute = blueprint[value["__class__"]](**value)
-                        self.__objects[key] = attribute
+                        FileStorage.__objects[key] = attribute
         except FileNotFoundError:
             pass
